f406dd8ed58fc1776d6c60bb833b4ebfe62c7cfb
tcox5698 <tcox56_98@yahoo.com>
2017-08-05 08:58:51 -0500

    test invisibility of others sites

--- features/sites.feature
+++ features/sites.feature
@@ -8,11 +8,11 @@ Feature: Manage sites
     Then I can edit the name of site 'My New Site'
     And I can delete the site 'My New Site'
 
-#  Scenario: Other users can not see or edit a site
-#    Given 'bob@example.com' has created a site 'Bob Site'
-#    And I am logged in as 'nancy@example.com'
-#    When I visit the Sites page
-#    Then I do not see 'Bob Site'
+  Scenario: Other users can not see or edit a site
+    Given 'bob@example.com' has created a site 'Bob Site'
+    And I am logged in as 'nancy@example.com'
+    When I visit the Sites page
+    Then I do not see 'Bob Site'
 #    When I create a new Site 'Nancy Site'
 #    Then I see 'Nancy Site' in the list of Sites
 #    And I do not see 'Bob Site'
--- features/step_definitions/sites_steps.rb
+++ features/step_definitions/sites_steps.rb
@@ -3,20 +3,28 @@ Then(/^I see the Sites page$/) do
 end
 
 And(/^I visit the Sites page$/) do
-  click_link 'Sites'
+  visit_sites_page
 end
 
-When(/^I create a new Site '(.*)'$/) do |site_name|
-  click_link 'Sites'
+def create_site(site_name)
+  visit_sites_page
   click_link 'New Site'
   page.fill_in 'Name', :with => site_name
   page.click_on 'Create Site'
   expect(page).to have_content('Site was successfully created.')
 end
 
-Then(/^I see '(.*)' in the list of Sites$/) do |site_name|
+When(/^I create a new Site '(.*)'$/) do |site_name|
+  create_site(site_name)
+end
+
+def visit_sites_page
   click_link 'Sites'
   expect(page.find(:xpath, '//h1')).to have_content('Sites')
+end
+
+Then(/^I see '(.*)' in the list of Sites$/) do |site_name|
+  visit_sites_page
   expect(page).to have_xpath("//td[.='#{site_name}']")
 end
 
@@ -25,7 +33,7 @@ Then(/^I can edit the name of site '(.*)'$/) do |site_name|
   page.fill_in 'Name', :with => "modified #{site_name}"
   click_button 'Update Site'
   expect(page).to have_content('Site was successfully updated.')
-  click_link 'Sites'
+  visit_sites_page
   expect(page).to have_xpath("//td[.='modified #{site_name}']")
   click_link 'Edit'
   page.fill_in 'Name', :with => "#{site_name}"
@@ -33,10 +41,20 @@ Then(/^I can edit the name of site '(.*)'$/) do |site_name|
 end
 
 And(/^I can delete the site '(.*)'$/) do |site_name|
-  click_link 'Sites'
+  visit_sites_page
   destroy_link = page.find(:xpath, "//tbody/tr[td[.='#{site_name}']]/td[4]/a")
   accept_confirm do
     destroy_link.click
   end
   expect(page).to have_content('Site was successfully destroyed.')
+end
+
+Given(/^'(.*)' has created a site '(.*)'$/) do |user_name, site_name|
+  register_and_login(user_name)
+  create_site(site_name)
+  logout
+end
+
+Then(/^I do not see '(.*)'$/) do |site_name|
+  expect(page).not_to have_content(site_name)
 end
