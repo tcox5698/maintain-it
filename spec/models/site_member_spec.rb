767db9600d319078a9f64137ebe1c9eee403fdaa
tcox5698 <tcox56_98@yahoo.com>
2017-08-06 22:27:58 -0500

    test sitemembers to sites and users relationship, add factorygirl

--- spec/models/site_member_spec.rb
+++ spec/models/site_member_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.describe SiteMember, type: :model do
   context 'when created with user and site' do
-    before do
-      user = User.create()
+    let(:user) {FactoryGirl.create(:user)}
+    let(:site) {FactoryGirl.create(:site)}
+    subject {SiteMember.new(attributes={ user: user, site: site, nick_name: 'fake nickname' })}
+    
+    it 'has the input site' do
+      expect(subject.site).to be site
     end
-    it 'has the input site'
-    it 'has the input user'
+
+    it 'has the input user' do
+      expect(subject.user).to be user
+    end
+    
+    it 'has the nick name' do
+      expect(subject.nick_name).to eq 'fake nickname'
+    end
   end
 end
