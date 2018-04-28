008e2378a4b98b3cdc38d22faedefc57a4c542ee
tcox5698 <tcox56_98@yahoo.com>
2018-04-28 08:42:37 -0500

    check in existing member

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_10_013738) do
+ActiveRecord::Schema.define(version: 2018_04_23_030001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_04_10_013738) do
     t.string "role", default: "visitor", null: false
     t.string "status", default: "absent", null: false
     t.index ["site_id"], name: "index_site_members_on_site_id"
+    t.index ["user_id", "site_id"], name: "index_site_members_on_user_id_and_site_id", unique: true
     t.index ["user_id"], name: "index_site_members_on_user_id"
   end
 
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Site, type: :model do
   it 'validates name is not blank' do
-    expect {Site.create!(attributes={ name: '' })}.to raise_error 'Validation failed: Name can\'t be blank'
+    expect {Site.create!(attributes = { name: '' })}.to raise_error 'Validation failed: Name can\'t be blank'
   end
 
   it 'lists its site_members' do
@@ -10,24 +10,62 @@ RSpec.describe Site, type: :model do
     expect(Site.first.site_members).to eq [site_member]
   end
 
-  describe "check_in_new_user" do
+  describe "check_in_user" do
     let(:site) {FactoryBot.create(:site)}
     let(:input_email) {"input@example.com"}
 
-    before do
-      site.check_in_user(email:input_email)
-    end
+    context "when user does not exist" do
+      before do
+        site.check_in_user(email: input_email)
+      end
 
-    it "creates a new user" do
-      user = User.find_by_email(input_email)
-      expect(user.email).to eq input_email
-    end
-
-    describe "user" do
-      it "has a site_member associated to the site" do
+      it "creates a new user" do
         user = User.find_by_email(input_email)
-        actual_site = user.site_members.first.site
-        expect(actual_site.id).to eq site.id
+        expect(user.email).to eq input_email
+      end
+
+      describe "user" do
+        it "has a site_member associated to the site" do
+          user = User.find_by_email(input_email)
+          actual_site = user.site_members.first.site
+          expect(actual_site.id).to eq site.id
+        end
+      end
+    end
+
+    context "when user exists" do
+      let(:existing_user) {FactoryBot.create(:user, email: input_email)}
+
+      context "when site member does not exist" do
+        before do
+          expect(existing_user.email).to eq input_email
+          site.check_in_user(email: input_email)
+        end
+
+        describe "user" do
+          it "has a site_member associated to the site" do
+            user = User.find_by_email(input_email)
+            actual_site = user.site_members.first.site
+            expect(actual_site.id).to eq site.id
+          end
+        end
+      end
+      
+      context "when site member exists" do
+        let(:existing_member) {FactoryBot.create(:site_member, user: existing_user, site: site)}
+
+        before do
+          expect(existing_member.nick_name).to eq "FactoryBotSiteMember"
+          site.check_in_user(email: input_email)
+        end
+
+        describe "user" do
+          it "has a site_member associated to the site" do
+            user = User.find_by_email(input_email)
+            actual_site = user.site_members.first.site
+            expect(actual_site.id).to eq site.id
+          end
+        end
       end
     end
   end
