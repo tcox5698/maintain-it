78bab18180d22f3d26627d2e5b944bbccd44899e
tcox5698 <tcox56_98@yahoo.com>
2018-05-12 08:38:49 -0500

    first part of test works

--- features/step_definitions/chore_steps.rb
+++ features/step_definitions/chore_steps.rb
@@ -4,22 +4,41 @@ end
 
 
 And(/^I enter the following chore information and save$/) do |table|
-  # table is a table.hashes.keys # => [:Name, :Description]
   inputs = table.hashes.first
-  page.fill_in 'chore[name]', :with => inputs[:Name]
-  page.fill_in 'chore[description]', :with => inputs[:Description]
+  page.fill_in 'chore[name]', :with => inputs[:ChoreName]
+  page.fill_in 'chore[description]', :with => inputs[:ChoreDescription]
   page.click_on 'Create Chore'
   expect(page).to have_content('Chore was successfully created.')
 end
 
 
-Then(/^I see this list of chores for site "([^"]*)"$/) do |site_name, table|
-  # table is a table.hashes.keys # => [:Name, :Description, :Schedule]
+def assert_chores_displayed(table)
   table.hashes.each do |expected|
-    expect(page).to have_content expected[:Name]
-    actual = find(:xpath, "//tbody/tr[td[contains(.,'#{expected[:Name]}')]]")
+    expect(page).to have_content expected[:ChoreName]
+    actual = find(:xpath, "//tbody/tr[td[contains(.,'#{expected[:ChoreName]}')]]")
     expect(actual).not_to be_nil
-    expect(actual).to have_content expected[:Description]
+    expect(actual).to have_content expected[:ChoreDescription]
     expect(actual).to have_content expected[:Schedule]
   end
+end
+
+Then(/^I see this list of chores for site "([^"]*)"$/) do |site_name, table|
+  # table is a table.hashes.keys # => [:ChoreName, :ChoreDescription, :Schedule]
+  assert_chores_displayed(table)
+end
+
+
+And(/^Site Host "([^"]*)" has created the following chores$/) do |email, table|
+  # table is a table.hashes.keys # => [:SiteName, :ChoreName, :ChoreDescription, :ChoreSchedule]
+
+  # create the chores
+  table.hashes.each do |chore|
+    site = Site.find_by! name: chore[:SiteName]
+
+    Chore.create!(name: chore[:ChoreName], description: chore[:ChoreDescription],
+                  schedule: chore[:ChoreSchedule].downcase, site: site)
+  end
+
+  visit("/chores")
+  assert_chores_displayed(table)
 end
