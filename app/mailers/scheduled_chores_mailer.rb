18c9e37a11573a485aeb86bf7d1691dbd31f0852
tcox5698 <tcox56_98@yahoo.com>
2019-11-24 13:42:51 -0600

    fix mailer

--- app/mailers/scheduled_chores_mailer.rb
+++ app/mailers/scheduled_chores_mailer.rb
@@ -3,20 +3,14 @@ class ScheduledChoresMailer < ApplicationMailer
   def chores_email
     site = params[:site]
     Rails.logger.info "ScheduledChoresMailer GOT SITE: #{site.inspect}"
-
     @scheduled_chores = site.scheduled_chores
 
     Rails.logger.info "ScheduledChoresMailer GOT SCHEDULED CHORES"
+    enabled_members = site.site_members.select { |m| m.notification_channel_enabled?('email') }
 
-    site.site_members.each do |member|
-      Rails.logger.info "ScheduledChoresMailer queueing email to: #{member.user.email}"
-
-      enabled_members = site.site_members.select { |m| m.notification_channel_enabled?('email') }
-
-      mail(
-        bcc: enabled_members.map { |m| m.user.email },
-        subject: "MaintainIt! It's time to get some chores done!"
-      )
-    end
+    mail(
+      bcc: enabled_members.map { |m| m.user.email },
+      subject: "MaintainIt! It's time to get some chores done!"
+    )
   end
 end
